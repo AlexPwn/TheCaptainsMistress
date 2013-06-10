71621fd837bf2e51d8c14c2bd19c18b935f60b2b
Twan <twanverboom@outlook.com>
2013-06-10 15:33:54 +0200

    Changed some of the layout and added a javascript file.

--- src/tictactoe/view.clj
+++ src/tictactoe/view.clj
@@ -2,20 +2,33 @@
   (:use hiccup.form
         [hiccup.def :only [defhtml]]
         [hiccup.element :only [link-to]]
-        [hiccup.page :only [html5 include-css]])
+        [hiccup.page :only [html5 include-css include-js]])
   (:require [tictactoe.model :as model]))
 
 (defhtml layout [& content]
   (html5
    [:head
     [:title "The Captain's Mistress"]
-    (include-css "/css/tictactoe.css")]
-   [:body [:div#holder [:div#title [:h1]] [:div#wrapper content]]
+    (include-css "/css/tictactoe.css")
+    (include-js "/js/lib.js")]
+   [:body [:div#holder [:div#title [:h1]] [:div#placeholders] [:div#wrapper content]]
     
     ]))
 
+(defn placeholders-html []
+   [:tr
+    [:td { :class (str "placeholdercol0") }]
+    [:td { :id (str "placeholdercol1") }]
+    [:td { :id (str "placeholdercol2") }]
+    [:td { :id (str "placeholdercol3") }]
+    [:td { :id (str "placeholdercol4") }]
+    [:td { :id (str "placeholdercol5") }]
+    [:td { :id (str "placeholdercol6") }]
+    [:td { :id (str "placeholdercol7") }]
+   ])
+
 (defn cell-html [rownum colnum cell with-submit?] 
-  [:td 
+  [:td { :class (str "col" colnum) }
    [:input {:name (str "b" rownum colnum) 
             :value (str cell)
             :type (if with-submit? 
@@ -29,16 +42,16 @@
   
 (defn board-html [board with-submit?]
   (form-to [:post "/"]
-           [:table 
+           [:table { :id "gameTable" }
+            (placeholders-html)
             (map-indexed (fn [rownum row]
                            (row-html rownum row with-submit?)) 
                          board)]))
 
 (defn play-screen []
   (layout
-    [:div 
      [:p "Captain " (model/get-player) ", it is your time to shine!"]
-     (board-html (model/get-board) true)]))
+     (board-html (model/get-board) true)))
 
 (defn winner-screen [winner]
   (layout
